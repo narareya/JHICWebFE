bfa9e58c75d38588ce6ccd665d366d6549c14a17
narareya <nomnomhiranami@gmail.com>
2025-10-15 12:47:50 +0700

    feat [EJL] images

---
+++ frontend/src/utils/jurusan.js
@@ -0,0 +1,28 @@
+const jurusan = {
+    'Animasi 2D' : {
+        'visi' : 'Menjadi SMK dengan Kompetensi Animasi 2D atau Animasi 3D yang mencetak artworker dan entrepreneur dengan level Asia Tenggara.',
+        'misi' : 'Membekali siswa/i Animasi 2D atau 3D dengan wawasan, pengetahuan, pengalaman, dan keahlian 2D atau 3D Animasi agar dapat menghasilkan portofolio yang diakui oleh industri animasi serta masyarakat publik.'
+    },
+
+    'Animasi 3D' : {
+        'visi' : 'Menjadi SMK dengan Kompetensi Animasi 2D atau Animasi 3D yang mencetak artworker dan entrepreneur dengan level Asia Tenggara.',
+        'misi' : 'Membekali siswa/i Animasi 2D atau 3D dengan wawasan, pengetahuan, pengalaman, dan keahlian 2D atau 3D Animasi agar dapat menghasilkan portofolio yang diakui oleh industri animasi serta masyarakat publik.',
+    },
+
+    'DG' : {
+        'visi' : 'Menjadi SMK dengan Program Keahlian DKV unggulan mencetak Designer dan Entrepreneur yang adaptif terhadap perkembangan teknologi global.',
+        'misi' : 'Mendidik, melatih dan membekali peserta didik dengan kreatifitas desain agar mampu menghasilkan karya sebagai portofolio yang diakui oleh Dunia Industri dan Dunia Usaha.',
+    },
+
+    'TG' : {
+        'visi' : 'Menjadi jurusan Desain Komunikasi Visual teknik grafika yang mampu Mewujudkan tenaga kerja professional mandiri, kreatif, berakhlaqul karimah serta mampu menciptakan terbentuknya lulusan siap kerja dan menciptakan lapangan kerja yang mandiri.',
+        'misi' : 'Menghasilkan Sumber Daya Manusia (SDM) yang memiliki kemampuan dalam bidang Desain Komunikasi Visual dengan wawasan global berbasis budaya local serta dilandasi iman dan taqwa dan menciptakan Lulusan yang disiplin, berdaya saing dan berkualitas dalam Bidang grafika',
+    },
+
+    'PPLG' : {
+        'visi' : 'Menjadi SMK dengan Kompetensi PPLG (Pengembangan Perangkat Lunak dan Gim) yang mencetak programmer dan frelancer dengan level Asia Tenggara',
+        'misi' : 'Membekali siswa-siswi PPLG (Pengembangan Perangkat Lunak dan Gim) dengan wawasan, pengetahuan, pengalaman, serta keahlian programming agar dapat menghasilkan karya dan portofolio yang diakui oleh industri teknologi informasi serta masyarakat publik',
+    },
+}
+
+export default jurusan;
